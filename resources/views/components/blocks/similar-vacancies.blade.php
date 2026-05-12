90b2a55721be68dbaccf0f0f61e76b7c354b3da1
ikromr04 <ikromr04@gmail.com>
2026-05-12 15:08:38 +0500

    logo to images

--- resources/views/components/blocks/similar-vacancies.blade.php
+++ resources/views/components/blocks/similar-vacancies.blade.php
@@ -4,30 +4,38 @@
 ])
 
 <section class="{{ $class ? "$class " : '' }}similar-vacancies">
-  <h2 class="similar-vacancies__title title">
-    @lang('Похожие вакансии по вашему запросу'):
-  </h2>
+    <h2 class="similar-vacancies__title title">
+        @lang('Похожие вакансии по вашему запросу'):
+    </h2>
 
-  <ul class="list-none p-0 m-0 grid gap-10 md:grid-cols-2 lg:grid-cols-3">
-    @foreach ($vacancies as $vacancy)
-      <li class="flex">
-        <a class="flex flex-col text-inherit no-underline py-8 px-6 border rounded-[20px] border-[#C4C9CE]" href="{{ route('pages.vacancy', $vacancy->id) }}">
-          <div class="vacancy-card__title !text-[20px] font-bold">{{ strip_tags($vacancy->translation?->title)}}</div>
-          <div class="vacancy-card__description">{{ preg_replace('/[^\p{L}\p{N}\s\.,!?-]/u', '', strip_tags($vacancy->translation?->description)) }}</div>
+    <ul class="list-none p-0 m-0 grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+        @foreach ($vacancies as $vacancy)
+            <li class="flex">
+                <a class="flex flex-col text-inherit no-underline py-8 px-6 border rounded-[20px] border-[#C4C9CE]" href="{{ route('pages.vacancy', $vacancy->id) }}">
+                    <div class="vacancy-card__title !text-[20px] font-bold">{{ strip_tags($vacancy->translation?->title) }}</div>
+                    <div class="vacancy-card__description">{{ preg_replace('/[^\p{L}\p{N}\s\.,!?-]/u', '', strip_tags($vacancy->translation?->description)) }}</div>
 
-          <div class="flex justify-between items-center mt-auto">
-            <div class="flex items-center gap-2 text-[14px] text-[#73787D]">
-              <svg class="text-primary" width="16" height="20">
-                <use xlink:href="#location" />
-              </svg>
-              {{ $vacancy->translation?->city }}
-            </div>
-            <div class="flex items-center gap-2">
-              <img class="w-[80px] h-6 object-contain" src="{{ asset('/storage/'.$vacancy->company?->translation?->logo) }}" width="80" height="24" alt="{{ $vacancy->company?->translation?->title }}">
-            </div>
-          </div>
-        </a>
-      </li>
-    @endforeach
-  </ul>
+                    <div class="flex justify-between items-center mt-auto">
+                        <div class="flex items-center gap-2 text-[14px] text-[#73787D]">
+                            <svg class="text-primary" width="16" height="20">
+                                <use xlink:href="#location" />
+                            </svg>
+                            {{ $vacancy->translation?->city }}
+                        </div>
+                        <div class="flex items-center gap-2">
+                            @if ($vacancy->company?->translation?->logo)
+                                <img
+                                    class="w-[80px] h-6 object-contain"
+                                    src="{{ asset('/storage/' . $vacancy->company?->translation?->logo) }}"
+                                    width="80"
+                                    height="24"
+                                    alt="{{ $vacancy->company?->translation?->title }}"
+                                >
+                            @endif
+                        </div>
+                    </div>
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </section>
